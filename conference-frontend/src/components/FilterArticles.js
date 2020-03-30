329da4994e2e72d27da070b4b4d9fd3efe61f840
Maria Popa <maria.popasb@gmail.com>
2020-03-30 13:35:25 +0300

    add filter after domain

--- conference-frontend/src/components/FilterArticles.js
+++ conference-frontend/src/components/FilterArticles.js
@@ -5,14 +5,13 @@ import { withRouter } from 'react-router-dom';
 import DataTable from "react-data-table-component";
 import axios from 'axios';
  
-const mockData = [
-  { 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },{ 'author': 'Ana', 'domain': 'Ma', 'title': 'aaa', },
-  { 'author': 'Annnnna', 'domain': 'Mamm', 'tile': 'aahhgha' }];
 const options = [
-  { value: 'firstName', label: 'First Name' },
-  { value: 'lastName', label: 'Last Name' },
+  { value: 'author', label: 'Author(First Name + Last Name)' },
   { value: 'domain', label: 'Domain' },
   { value: 'title', label: 'Title' }]
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTg1NTY0MDkxLCJleHAiOjE1ODY0MjgwOTF9.BpOcah-IirZu4dhu4NLt5ROfQj3NLo_WB6sM_4uzy5zeV5or6lkvbPvnri4xa_jqsLZ7vL5KIkgRu-U133zdvQ"
+
+
 class FilterArticles extends Component {
   constructor(props) {
     super(props);
@@ -23,48 +22,67 @@ class FilterArticles extends Component {
     }
 
   }
-  componentDidMount() {
-    // axios.get('http://localhost:8080/conference/articles/')
-    //   .then(res => {
-    //     console.log(res);
-    //     const articles = res.data;
-
-    //     this.setState({ data: articles });
-
-    //   })
+  componentWillMount() {
+    axios.get('http://localhost:8080/conference/articles/',{
+      headers: {
+        Authorization: `Bearer ${token}` 
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        // const articles = res.data;
+        this.setState({data:res.data});
+        this.setState({ok:true});
+        console.log(this.state.data);
+      })
     console.log("Prima data intru aici");
   }
 
   handleFilter = () => {
-    //var param = this.selectedOption.label;
-    // axios.get('http://localhost:8080/conference/articles/',
-    // {
+    if(this.state.selectedOption.label === "Domain" )
+    {
+      axios.get('http://localhost:8080/conference/articles/domain/'+this.refs.filter_input.value, 
+      {
+        headers: {
+          Authorization: `Bearer ${token}` 
+        }
+      })
+        .then(res => {
+          console.log(res);
+          this.setState({ data: res.data });
+          this.state.ok = true;
+        })
+    }
+    if(this.state.selectedOption.value === "author") {
+      var firstName = this.refs.filter_input.value.split(" ")[0];
+      console.log(firstName + firstName.length);
       
-    // })
-    //   .then(res => {
-    //     console.log(res);
-    //     const articles = res.data;
-
-    //     this.setState({ data: articles });
-    //     this.state.ok = true;
-    //   })
+      var lastName = this.refs.filter_input.value.split(" ")[1];
+      console.log(lastName + lastName.length);
+      axios.get('http://localhost:8080/conference/articles/author',
+      {
+        params:{
+        "firstName" : firstName,
+        "lastName" : lastName
+        },
+        headers: {
+           Authorization: `Bearer ${token}` 
+        }
+      })
+        .then(res => {
+          console.log(res);
+          this.setState({ data: res.data });
+          this.state.ok = true;
+        })
+    }
   }
-
-  // sendData = () => {
-
-  //     };
-
-  //     // axios.post(apiLoginUrl, payload).then((response) => {
-  //     //     // TODO 
-  //     //     alert(response.status);
-  //     // });
-  // }
 
   render() {
 
     return (
+      
       <div className="centered">
-
+      
         <div style={{ height: '700px' }} className="demo-card-wide mdl-card mdl-shadow--5dp">
           <div className="mdl-card__title">
             <h2 className="mdl-card__title-text">Filter Articles</h2>
@@ -74,23 +92,25 @@ class FilterArticles extends Component {
               className="basic-single"
               options={options}
               onChange={(value) => {
-                this.setState({ selectedOption: value }, () => {
-                  this.handleFilter();
-                })
+                this.setState({ selectedOption: value })
               }}
             />
+         
+          
+          <input style={{ width: '200px' }} className="mdl-text-field__input" type="text" id="filter_input" ref="filter_input" />
+          <button className="mdl-button mdl-js-button mdl-button--raised" onClick={this.handleFilter}>Filter</button>
           </div>
 
 
-
+          { this.state && this.state.data &&
 
           <DataTable
           title="Articles"
-            data={mockData}
+            data={this.state.data}
             columns={[
               {
-                name: "Author",
-                selector: "author",
+                name: "Tile",
+                selector: "title",
                 sortable: true,
               },
               {
@@ -98,26 +118,29 @@ class FilterArticles extends Component {
                 selector: "domain",
                 sortable: true,
               },
-              {
-                name: "Abstract",
-                selector: "abstract",
-                sortable: true,
+              { 
+                name:"First Name",
+                selector: "author",
+                cell: d => d.author.firstName 
               },
               {
-                name: "Title",
-                selector: "title",
-                sortable: true,
+                name:"Last Name",
+                selector: "author",
+                cell: d => d.author.lastName
               }
             ]}
             fixedHeader
             fixedHeaderScrollHeight="300px"
 
           />
-
+          }
         </div>
+      
       </div>
-
+          
+          
     );
-  }
+    }
+
 };
 export default FilterArticles;
