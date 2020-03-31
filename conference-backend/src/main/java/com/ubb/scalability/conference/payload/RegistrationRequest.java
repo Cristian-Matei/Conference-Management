d924be6b5d10e5ba736b4afac8a77d36cafbb13e
kedith <kedithke98@gmail.com>
2020-03-31 21:18:06 +0300

    Change post and add body.

---
+++ conference-backend/src/main/java/com/ubb/scalability/conference/payload/RegistrationRequest.java
@@ -0,0 +1,12 @@
+package com.ubb.scalability.conference.payload;
+
+import lombok.Data;
+
+@Data
+public class RegistrationRequest {
+
+    private Integer talkId;
+
+    private Integer userId;
+
+}
